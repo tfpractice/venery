0544eda727a9b7c0086803b9cfe901b4c4f071d2
theory friction practice <jackson.brebnor@gmail.com>
2017-08-16 10:38:14 -0400

    background image

--- src/components/main.js
+++ src/components/main.js
@@ -1,31 +1,41 @@
-import React, { Component, } from 'react';
-import { connect, } from 'react-redux';
-import { Route, Switch, } from 'react-router-dom';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 
 import About from './about';
 import Nav from './nav';
-import { Game, } from './game';
+import { Game } from './game';
 
-const styles = { paddingTop: '5rem', };
-
-const stateToProps = ({ game: { inPlay, }, }) => ({ inPlay, });
+const styles = {
+    // backgroundPosition: 'center',
+    // backgroundImage: `url('/images/butterfly.jpg')`,
+    // backgroundSize: 'cover',
+    // backgroundRepeat: 'no-repeat',
+    paddingTop: '5rem',
+    // backgroundColor: 'rgba(128,128,128,0.8)',
+};
+// export const pjStyle = url => ({
+//
+// });
+const stateToProps = ({ game: { inPlay } }) => ({ inPlay });
 
 class Main extends Component {
-  render() {
-    // console.log('this.prop', this.props);
-    return (
-      <Grid container justify="center" style={styles} >
-        <Nav />
-        <Grid item xs={11} sm={12}>
-          <Switch >
-            <Route path="/play" component={Game} />
-            <Route path="/" exact component={About} />
-          </Switch>
-        </Grid>
-      </Grid>
-    );
-  }
+    render() {
+        return (
+            <Grid container justify="center" style={styles}>
+                <Grid item xs={12}>
+                    <Nav />
+                </Grid>
+                <Grid item xs={12} sm={11}>
+                    <Switch>
+                        <Route path="/play" component={Game} />
+                        <Route path="/" exact component={About} />
+                    </Switch>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 export default connect(stateToProps, Game.actions)(Main);
